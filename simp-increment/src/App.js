d0aec71c378f15a23866859b2c4959b1f4cf36e3
David <DavidHowardJr07@gmail.com>
2022-06-23 10:05:58 -0500

    Done

--- simp-increment/src/App.js
+++ simp-increment/src/App.js
@@ -1,11 +1,21 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h3>Hello</h3>
+const [state, setState] = useState(0)
+
+const counter = state
+
+const increment = () => {
+  setState(counter + 1)
+}
+
+  return(
+    <div>
+      <p>{counter}</p>
+      <button onClick={increment}>Increment</button>
     </div>
-  );
+  )
 }
 
 export default App;
