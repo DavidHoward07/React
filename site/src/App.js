16511f58250c39c00f7b01606b38a20bf03ee23b
David <DavidHowardJr07@gmail.com>
2022-06-26 10:47:40 -0500

    Workin on it

--- site/src/App.js
+++ site/src/App.js
@@ -3,10 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola Amigo</h1>
+    <div>
+      <nav className="navbar background">
+        <ul className="nav-list">
+          <div className="logo">
+            <img src="https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1439,w_2560,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1492181001/articles/2015/05/06/morgan-freeman-shoots-straight-on-legalizing-marijuana-and-his-escape-from-new-york/150505-stern-morgan-freeman-tease_zomwym" alt="me"/>
+          </div>
+          <li><a href="#Doodoo">Doodoo</a></li>
+          <li><a href="#Hello">Hello</a></li>
+          <li><a href="#What">What</a></li>
+          <li><a href="#Shit">Shit</a></li>
+        </ul>
+
+        <div className="rightNav">
+
+        </div>
+      </nav>
     </div>
   );
-}
+};
 
 export default App;
